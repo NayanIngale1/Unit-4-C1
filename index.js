49e457d249c720477eb311eb922e63b11b36e529
NayanIngale1 <nayaningale15@gmail.com>
2022-03-07 12:33:42 +0530

    final changes

--- index.js
+++ index.js
@@ -7,35 +7,31 @@ app.get("/books", (req, res) => {
   res.send({ route: req.path });
 });
 
-
-app.use(checkPermission);
-
-
-app.get("/libraries", (req, res) => {
-    res.send({ route: req.path, permission: req.permission});
+app.get("/libraries", checkPermission("librarian"), (req, res) => {
+  res.send({ route: req.path, permission: req.permission });
 });
 
-app.get("/authors", (req, res) => {
+app.get("/authors", checkPermission("author"), (req, res) => {
   res.send({ route: req.path, permission: req.permission });
 });
 
 function logger(req, res, next) {
   console.log("request path", req.path);
-    next();
+  next();
 }
 
-function checkPermission(req, res, next) {
-    if (req.path == '/libraries') {
-        req.permission = true;
+function checkPermission(role) {
+    return function check(req, res, next) {
+    if (req.path == "/libraries") {
+      req.permission = true;
     }
-    if (req.path == '/authors') {
-        req.permission = true;
+    if (req.path == "/authors") {
+      req.permission = true;
     }
     next();
+  }
 }
 
 app.listen(4000, () => {
-
   console.log("listening on port 4000");
-
 });
